203c564b25ee2bc461518d38466d36e205704b1e
Jaybird <jrmiller022@gmail.com>
2018-12-19 10:29:05 -0600

    trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,8 @@
 //Set game to start.
-$(document).ready(function(){
-  
-});
+$(document).ready(function() {});
 
 //Set variables
-
+var userPick;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
@@ -13,70 +11,100 @@ var IntervalId;
 var clockRunning = false;
 var answer;
 var choices;
-
 var questions = [{
-    question: "What is the name of the Father?",
-    choices: ["Mark", "Jessie", "Joey", "Danny" ],
-    answer: "Danny", 
-},
-{
-    question: "What the name of the dog?",
-    choices: ["Commet", "Pup", "Lucky", "Driver" ],
-    answer: "Commet",
-},
-{
-    question: "Who did Jessy Marry?",
-    choices: ["Kimmy", "Sara", "Becky", "Whitney" ],
-    answer: "Becky",
-},
-{    
-    question: "What did Joey do for a profession?",
-    choices: ["Musician", "Cook", "comic", "TV show host" ],
-    answer: "comic"
+	question: "What is the name of the Father?",
+	choices: ["Mark", "Jessie", "Joey", "Danny"],
+	answer: "Danny",
+}, {
+	question: "What the name of the dog?",
+	choices: ["Commet", "Pup", "Lucky", "Driver"],
+	answer: "Commet",
+}, {
+	question: "Who did Jessy Marry?",
+	choices: ["Kimmy", "Sara", "Becky", "Whitney"],
+	answer: "Becky",
+}, {
+	question: "What did Joey do for a profession?",
+	choices: ["Musician", "Cook", "comic", "TV show host"],
+	answer: "comic"
 }]
-// Displays questions.
-for(var i = 0; i < questions.length; i++) {
-    $("#questions").append(questions[i].question + "<br>");
 
-// Displays my Choices to pick from.
-    var choice = questions[i].choices;
-    for(var j = 0; j < choice.length; j++) {
-        $(".choices").append(choice[j] + "<br>");
-        }
+// Displays questions.
+question = $("<div>");
+$("#quiz").append(question);
+for (var i = 0; i < questions.length; i++) {
+	$(question).append("<p>" + questions[i].question + "</p>");
+	// Displays my Choices to pick from.
+	var choice = questions[i].choices;
+	for (var j = 0; j < choice.length; j++) {
+		var button = $("<button>");
+		button.attr("class", "answerButton");
+		$(question).append(button);
+		$(button).append(choice[j]);
+	}
 }
+
+$(".choices").on("click", "button", function() {
+	userPick = $(".choices").text(choices);
+	questions[i].val;
+	if (userPick !== choices[j].val) {
+		incorrect++;
+	} else(userPick === choices[j].val);
+	correct++;
+})
 
 //Timer function
 function decrement() {
 	timer--;
-    $("#time-left").text("Time Left: " + timer);
-    if (timer === 0){
-        stop();
-    }
+	$("#time-left").text("Time Left: " + timer);
+	if (timer === 0) {
+		stop();
+	}
 };
 setTimeout(thirtySeconds, 1000 * 20);
 
 function thirtySeconds() {
 	$("#time-left").html("Time Left" + timer);
 	console.log(timer);
-};
+}
 $("#start").on("click", gameStart)
 
 function gameStart() {
 	clearInterval(IntervalId);
 	IntervalId = setInterval(decrement, 1000);
-    timer = 3;	
+	timer = 3;
     stop();
-}
     
-//Stop timer function
-function stop() {
-    if (timer <= 0) {
-    clearInterval(IntervalId);
-    clockRunning = false;
-    }
+// loop through correctArray & radioName to match html elements & answers
+	for (var i = 0; i < 10; i++) {
+		if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+			correct++;
+			console.log("this is correct! number:" + i)
+		} else {
+			incorrect++;
+			console.log("this is wrong! number:" + i)
+		};
+	}
+	$('#correctTimesUp').append(correctAnswers);
+	// display wrongAnswers
+	$('#wrongTimesUp').append(wrongAnswers);
+	$('#timesUp').fadeIn(1000).show();
+	// alert("Times Up!");
+	clearInterval(timer);
+	return;
 }
 
-  function reset() {
-    timer = 30;
-    $("#timer").text(timer);
-  }
+//Stop timer function
+function stop() {
+	if (timer <= 0) {
+		clearInterval(IntervalId);
+		clockRunning = false;
+	}
+}
+
+function reset() {
+	timer = 30;
+	$("#timer").text(timer);
+}
+
+
